75a254dd6be7d83170d60be2fe24fbcca96dd6b8
Božidar Labaš <bozidar.labas@fiveminutes.eu>
2017-06-13 15:58:48 +0200

    Add test class

---
+++ iOSViews/Classes/UICollectionView/Test.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  Pods
+//
+//  Created by Božidar on 13/06/2017.
+//
+//
+
+import Foundation
+
+public class Test {
+
+    func getTest() -> String {
+        return "Test"
+    }
+}
